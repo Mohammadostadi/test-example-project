4ebb96029060460884cf4116841831497113df58
Mohammadostadi <mohammad.asram2000@gmail.com>
2024-05-05 00:48:14 +0330

    section products view test number 1

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,10 +1,18 @@
-
+const listCategory = []
+const menuContainer = document.querySelector('#categoryContainer');
 fetch('https://dummyjson.com/products')
 .then(res => res.json())
 .then(data =>{
 
     const product = data.products
     product.forEach((el) => {
-            console.log(el.category);
+        listCategory.push(el.category)
     });
-});
+}).then(() =>{
+    const setListCategory = new Set(listCategory)
+    setListCategory.forEach(el =>{
+        const html = `<li class="px-2 non-active-products"><a href="#" id="${el}">${el}</a></li>`
+        menuContainer.insertAdjacentHTML('beforeend', html)
+    })
+}
+);
